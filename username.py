25202ae39b337696eac05ceb0f50bc1f88643f16
Ramesh Timilsina <rameshtimilsina@users.noreply.github.com>
2022-04-18 18:26:07 +0000

    added username.py initial code

---
+++ username.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Ask the user to input their name
+
+name = input ("Please enter your name: ")
+
+# An input asking what day of the week it is
+
+day = input ("What day of the week it is: ")
+
+#print ("Hello " , name +"!" , " Happy", day)
+
+#Using f string
+print(f"Hello, {name}! Happy {day}!")
