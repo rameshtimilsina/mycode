9e1aeb6e5d3a4c6bb8eaba52fb10cb22d46727f9
Ramesh Timilsina <rameshtimilsina@users.noreply.github.com>
2022-04-20 03:39:19 +0000

    working on if, elif, else logic

---
+++ iftest2/ipstatic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+ipchk = input("Apply an IP address: ") #prompt user for input
+
+# a string tests as True
+if ipchk == "192.168.70.1":
+    print ("Looks like the IP address of the Gateway was set: " +ipchk + " This is not recommended")
+elif ipchk:
+    print("Looks like the IP address was set: " + ipchk)
+else:
+    print ("You did not provide input.")
+    
+
